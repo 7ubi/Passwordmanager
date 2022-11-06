a647cea7aa8f66ad0e77c6a7eb3e7cf222ab0346
Tobias Renner <tobi.renner03@gmail.com>
2022-11-06 15:52:48 +0100

    hide password

--- frontend/src/components/passwordmanager/PasswordStorage.js
+++ frontend/src/components/passwordmanager/PasswordStorage.js
@@ -11,7 +11,10 @@ import {
     TableHead,
     TableRow
 } from "@mui/material";
+import CircleIcon from '@mui/icons-material/Circle';
 import LogoutIcon from '@mui/icons-material/Logout';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 import AddCircleOutline from '@mui/icons-material/AddCircleOutline';
 import PasswordCreation from "./PasswordCreation";
 
@@ -23,6 +26,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.body}`]: {
         backgroundColor: '#240090',
         color: theme.palette.common.white,
+        width: "max-content"
     },
 }));
 
@@ -38,11 +42,18 @@ const PasswordStorage = ({  }) => {
         const response = await fetch("/api/passwordUser");
         const data = await response.json();
         data.map(password => {
+            password.showPassword = false;
             setPasswords(passwords => [...passwords, password]);
         })
     }
 
+    const togglePasswordShown = (password) => {
+        password.showPassword = !password.showPassword;
+        setPasswords(passwords => [...passwords]);
+    }
+
     const addPassword = (password) => {
+        password.showPassword = false;
         setPasswords(passwords => [...passwords, password])
     }
 
@@ -61,7 +72,7 @@ const PasswordStorage = ({  }) => {
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <StyledTableCell align="center" colSpan={2}>
+                            <StyledTableCell align="center" colSpan={3}>
                                 <h1>Password storage</h1>
                             </StyledTableCell>
                             <StyledTableCell align="center" colSpan={1}>
@@ -76,18 +87,42 @@ const PasswordStorage = ({  }) => {
                             <StyledTableCell align="center">Username</StyledTableCell>
                             <StyledTableCell align="center">Website</StyledTableCell>
                             <StyledTableCell align="center">Password</StyledTableCell>
+                            <StyledTableCell align="center" />
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {passwords.map((password) => (
                             <TableRow
                               key={password.title}
-                              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                                 <StyledTableCell align="center">{password.title}</StyledTableCell>
                                 <StyledTableCell align="center">{password.username}</StyledTableCell>
                                 <StyledTableCell align="center">{password.website}</StyledTableCell>
-                                <StyledTableCell align="center">{password.managed_password}</StyledTableCell>
+                                <StyledTableCell align="center" style={{width: '20%'}}>
+                                    <div style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                    }}>
+                                        {
+                                            password.showPassword ?
+                                            <span>{password.managed_password}</span>:
+                                            <span>
+                                                <CircleIcon className="iconBtn" style={{ fontSize: 'small', padding: 0}} />
+                                                <CircleIcon className="iconBtn" style={{ fontSize: 'small', padding: 0}} />
+                                                <CircleIcon className="iconBtn" style={{ fontSize: 'small', padding: 0}} />
+                                                <CircleIcon className="iconBtn" style={{ fontSize: 'small', padding: 0}} />
+                                            </span>
+                                        }
+                                    </div>
+                                </StyledTableCell>
+                                <StyledTableCell align="center" style={{width: '5%'}}>
+                                    {
+                                        password.showPassword ?
+                                        <VisibilityIcon className="iconBtn" onClick={ () => togglePasswordShown(password) } style={{ float: 'right'}} />:
+                                        <VisibilityOffIcon className="iconBtn" onClick={ () => togglePasswordShown(password) } style={{ float: 'right'}} />
+                                    }
+                                </StyledTableCell>
                             </TableRow>
                         ))}
                     </TableBody>
