eca526eacb09e81a59e67770101845c40546c83c
Tobias Renner <tobi.renner03@gmail.com>
2022-11-06 14:56:13 +0100

    better table and design

--- api/views/login_views.py
+++ api/views/login_views.py
@@ -5,7 +5,6 @@ from rest_framework.decorators import api_view
 
 from rest_framework.views import APIView
 from rest_framework import status
-from django.contrib.auth.models import User
 from rest_framework.response import Response
 from api.serializer import *
 
--- frontend/src/components/passwordmanager/PasswordStorage.js
+++ frontend/src/components/passwordmanager/PasswordStorage.js
@@ -1,8 +1,31 @@
 import React, { useState, useEffect } from "react";
-import {Grid, Modal} from "@mui/material";
+import {
+    Grid,
+    Modal,
+    Paper, styled,
+    Table,
+    TableBody,
+    TableCell,
+    tableCellClasses,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@mui/material";
+import LogoutIcon from '@mui/icons-material/Logout';
 import AddCircleOutline from '@mui/icons-material/AddCircleOutline';
 import PasswordCreation from "./PasswordCreation";
-import PasswordListElement from "./PasswordListElement";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: '#0C0032',
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        backgroundColor: '#240090',
+        color: theme.palette.common.white,
+    },
+}));
+
 
 const PasswordStorage = ({  }) => {
     const [open, setOpen] = useState(false);
@@ -12,7 +35,7 @@ const PasswordStorage = ({  }) => {
     const [passwords, setPasswords] = useState([]);
 
     const fetchPasswordsUser = async () => {
-        const response = await fetch("/api/passwordUser")
+        const response = await fetch("/api/passwordUser");
         const data = await response.json();
         data.map(password => {
             setPasswords(passwords => [...passwords, password]);
@@ -23,43 +46,53 @@ const PasswordStorage = ({  }) => {
         setPasswords(passwords => [...passwords, password])
     }
 
+    const logout = async () => {
+        const response = await fetch("/api/logout");
+        location.href = "/login";
+    }
+
     useEffect(() => {
-        fetchPasswordsUser()
+        fetchPasswordsUser();
     }, [])
 
     return (
         <Grid container spacing={1}>
-            <Grid item xs={12} align="center">
-
-            </Grid>
-            <Grid xs={12} align="center">
-
-            </Grid>
-            <Grid xs={12} align="center">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>
-                                <h1>
-                                    Password storage
-                                </h1>
-                            </th>
-                            <th align={"right"}>
-                                <button className="btn" onClick={ onOpen }>
-                                    <AddCircleOutline />
-                                </button>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            passwords.map(password =>
-                                <PasswordListElement password={ password } />
-                            )
-                        }
-                    </tbody>
-                </table>
-            </Grid>
+            <TableContainer className="passwordTable" component={ Paper }>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell align="center" colSpan={2}>
+                                <h1>Password storage</h1>
+                            </StyledTableCell>
+                            <StyledTableCell align="center" colSpan={1}>
+                                <AddCircleOutline className="iconBtn" onClick={ onOpen } />
+                            </StyledTableCell>
+                            <StyledTableCell align="center" colSpan={1}>
+                                <LogoutIcon className="iconBtn" onClick={ logout }/>
+                            </StyledTableCell>
+                        </TableRow>
+                        <TableRow>
+                            <StyledTableCell align="center">Title</StyledTableCell>
+                            <StyledTableCell align="center">Username</StyledTableCell>
+                            <StyledTableCell align="center">Website</StyledTableCell>
+                            <StyledTableCell align="center">Password</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {passwords.map((password) => (
+                            <TableRow
+                              key={password.title}
+                              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <StyledTableCell align="center">{password.title}</StyledTableCell>
+                                <StyledTableCell align="center">{password.username}</StyledTableCell>
+                                <StyledTableCell align="center">{password.website}</StyledTableCell>
+                                <StyledTableCell align="center">{password.managed_password}</StyledTableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
             <Modal
                 open={ open }
                 onClose={ onClose }
